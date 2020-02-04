be3b9c71fcf6aa8fbea995d4b07eea5b2b0cdf87
gft <gfhftht>
2020-02-04 17:02:31 +0700

    Added UserParams

---
+++ src/main/java/ru/cft/shift/quickstart_bus_traffic/params/UserParamsRequest.java
@@ -0,0 +1,124 @@
+package ru.cft.shift.quickstart_bus_traffic.params;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserParamsRequest {
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getParonym() {
+        return paronym;
+    }
+
+    public void setParonym(String paronym) {
+        this.paronym = paronym;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public List<String> getSubject() {
+        return subject;
+    }
+
+    public void setSubject(List<String> subject) {
+        this.subject = subject;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    public boolean isTeacher() {
+        return isTeacher;
+    }
+
+    public void setTeacher(boolean teacher) {
+        isTeacher = teacher;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String paronym;
+    private String faculty;
+    private int course;
+    private List<String> subject;
+    private String degree;
+    private String about;
+    private boolean isTeacher;
+    private int price;
+    private String photo;
+}
