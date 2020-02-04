b68f4efff642f39364a178b015234697876b0a2c
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-02-04 22:14:43 +0700

    Delete UserCriteria and update UserRepository with JpaSpecificationExecutor

--- src/main/java/ru/cft/shift/repetito/repository/UserRepository.java
+++ src/main/java/ru/cft/shift/repetito/repository/UserRepository.java
@@ -1,18 +1,11 @@
 package ru.cft.shift.repetito.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 import ru.cft.shift.repetito.entity.UserEntity;
-import ru.cft.shift.repetito.service.criteria.UserCriteria;
 
-import java.util.List;
 
-public interface UserRepository {
-    List<UserEntity> findUsers(UserCriteria userCriteria);
-
-    UserEntity findUserById(long id);
-
-    UserEntity updateUser(UserEntity user);
-
-    void deleteUserById(long id);
-
-    UserEntity createUser(UserEntity user);
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long>, JpaSpecificationExecutor<UserEntity> {
 }
