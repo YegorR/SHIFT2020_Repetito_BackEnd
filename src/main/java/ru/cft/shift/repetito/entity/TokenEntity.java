874c4618f0a4c5111268d9a9253fd4de57a17a2e
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-02-04 20:48:57 +0700

    Set TokenEntity

--- src/main/java/ru/cft/shift/repetito/entity/TokenEntity.java
+++ src/main/java/ru/cft/shift/repetito/entity/TokenEntity.java
@@ -1,7 +1,32 @@
 package ru.cft.shift.repetito.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import java.util.UUID;
 
 @Entity
 public class TokenEntity {
+    @Id
+    @OneToOne(fetch = FetchType.LAZY)
+    private UserEntity user;
+
+    private UUID uuid;
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
 }
