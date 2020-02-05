597a3dcee1115b5cf5f14532c408def22e219888
Viktor <MarkerVIktor@outlook.com>
2020-02-05 16:01:36 +0700

    update AuthenicationController

--- src/main/java/ru/cft/shift/repetito/controller/AuthenticationController.java
+++ src/main/java/ru/cft/shift/repetito/controller/AuthenticationController.java
@@ -1,9 +1,23 @@
 package ru.cft.shift.repetito.controller;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.cft.shift.repetito.params.LoginFormRequest;
+import ru.cft.shift.repetito.params.LoginResultResponse;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/login")
 public class AuthenticationController {
+    @RequestMapping("")
+    public LoginResultResponse login(LoginFormRequest loginFormRequest, HttpServletResponse response) {
+        LoginResultResponse loginResultResponse = new LoginResultResponse();
+        loginResultResponse.setSuccessful(true);
+        response.addCookie(new Cookie("ACCESS_TOKEN", UUID.randomUUID().toString()));
+        return loginResultResponse;
+    }
 }
