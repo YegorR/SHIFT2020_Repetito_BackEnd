6a797093c5d4e17a7457b58bf3351aebc607b325
Viktor <MarkerVIktor@outlook.com>
2020-02-05 15:15:58 +0700

    edit ReviewEntity

--- src/main/java/ru/cft/shift/repetito/entity/ReviewEntity.java
+++ src/main/java/ru/cft/shift/repetito/entity/ReviewEntity.java
@@ -1,7 +1,29 @@
 package ru.cft.shift.repetito.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Table;
+
+import static javax.persistence.GenerationType.IDENTITY;
 
 @Entity
+@Table(name="reviews")
 public class ReviewEntity {
+    @Column(name = "id")
+    @GeneratedValue(strategy = IDENTITY)
+    private int id;
+
+    @Column(name = "mark")
+    private int mark;
+
+    @Column(name = "comment")
+    private String  comment;
+
+    @Column(name = "reviewer_id")
+    private Long reviewerId;
+
+    @Column(name = "teacher_id")
+    private  Long teacherId;
+
 }
