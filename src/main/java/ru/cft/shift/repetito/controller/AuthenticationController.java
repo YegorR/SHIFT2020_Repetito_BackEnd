6c8b66b674683158ee25015d368d4bf0a780e2b0
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-02-05 14:18:44 +0700

    Set AuthenticationController with stub answer

--- src/main/java/ru/cft/shift/repetito/controller/AuthenticationController.java
+++ src/main/java/ru/cft/shift/repetito/controller/AuthenticationController.java
@@ -1,10 +1,22 @@
 package ru.cft.shift.repetito.controller;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.cft.shift.repetito.params.LoginFormRequest;
+import ru.cft.shift.repetito.params.LoginResultResponse;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/login")
 public class AuthenticationController {
-    
+    public LoginResultResponse login(LoginFormRequest loginFormRequest, HttpServletResponse response) {
+        LoginResultResponse loginResultResponse = new LoginResultResponse();
+        loginResultResponse.setSuccessful(true);
+        response.addCookie(new Cookie("ACCESS_TOKEN", UUID.randomUUID().toString()));
+        return loginResultResponse;
+    }
 }
