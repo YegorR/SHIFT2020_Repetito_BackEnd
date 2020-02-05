39b316942f7e7f360c6d106dcbbb7d5fc55f063d
gft <gfhftht>
2020-02-05 15:25:06 +0700

    A little bit changes

---
+++ src/main/java/ru/cft/shift/repetito/service/ReviewServiceImpl.java
@@ -0,0 +1,25 @@
+package ru.cft.shift.repetito.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.cft.shift.repetito.entity.ReviewEntity;
+import ru.cft.shift.repetito.repository.ReviewRepository;
+
+import java.util.List;
+
+@Service
+public class ReviewServiceImpl implements  ReviewService {
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Override
+    public List<ReviewEntity> get(Long teacher_id){
+        return reviewRepository.findReviewByRepetitorId(teacher_id);
+    }
+
+    @Override
+    public ReviewEntity add(ReviewEntity review)
+    {
+        return reviewRepository.createReview(review);
+    }
+}
