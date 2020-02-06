caa66c6684cf8ed82ece0c1aa5e3052be061b4e6
Ruslan <56080256+RuslanMac@users.noreply.github.com>
2020-02-06 16:27:46 +0700

    Add exception package

--- src/main/java/ru/cft/shift/repetito/exception/ApplicationException.java
+++ src/main/java/ru/cft/shift/repetito/exception/ApplicationException.java
@@ -7,7 +7,7 @@ public class ApplicationException extends Exception {
     }
     public ApplicationException(String message,Throwable throwable)
     {
-        super(message,throwable);
+         super(message,throwable);
     }
 
 }
