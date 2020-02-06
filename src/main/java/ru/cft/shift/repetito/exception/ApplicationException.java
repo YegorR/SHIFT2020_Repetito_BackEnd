e37a3d89fff8145fbfeb0fd2fcceea9b18ca8eab
Ruslan <56080256+RuslanMac@users.noreply.github.com>
2020-02-06 16:25:37 +0700

    Add ErrorControllerAdvice

---
+++ src/main/java/ru/cft/shift/repetito/exception/ApplicationException.java
@@ -0,0 +1,13 @@
+package ru.cft.shift.repetito.exception;
+
+public class ApplicationException extends Exception {
+    public ApplicationException(String message)
+    {
+        super(message);
+    }
+    public ApplicationException(String message,Throwable throwable)
+    {
+        super(message,throwable);
+    }
+
+}
