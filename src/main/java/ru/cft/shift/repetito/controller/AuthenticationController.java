572f945c9ebabe9767950de7e81e2fd537b22a58
Viktor <MarkerVIktor@outlook.com>
2020-02-06 12:58:11 +0700

    edits of AuthenticationService and Controller

--- src/main/java/ru/cft/shift/repetito/controller/AuthenticationController.java
+++ src/main/java/ru/cft/shift/repetito/controller/AuthenticationController.java
@@ -1,35 +1,54 @@
 package ru.cft.shift.repetito.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.cft.shift.repetito.entity.TokenEntity;
+import ru.cft.shift.repetito.entity.UserEntity;
 import ru.cft.shift.repetito.params.request.LoginFormRequest;
 import ru.cft.shift.repetito.params.response.LoginResultResponse;
 import ru.cft.shift.repetito.service.AuthenticationService;
-import javax.servlet.http.HttpServletResponse;
+import ru.cft.shift.repetito.service.TokenService;
+import ru.cft.shift.repetito.service.UserService;
+
 import java.io.IOException;
+import java.util.UUID;
 
 @RestController
-@RequestMapping("/login")
 public class AuthenticationController {
 
     @Autowired
     AuthenticationService authenticationService;
 
-    @RequestMapping("")
-    public LoginResultResponse login(@RequestBody LoginFormRequest loginFormRequest, HttpServletResponse httpServletResponse) throws IOException {
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    TokenService tokenService;
+
+    @RequestMapping("/login")
+    public ResponseEntity login(@RequestBody LoginFormRequest loginFormRequest){
         TokenEntity tokenEntity = authenticationService.login(loginFormRequest.getEmail(), loginFormRequest.getPassword());
         if (tokenEntity != null) {
             LoginResultResponse loginResultResponse = new LoginResultResponse();
             loginResultResponse.setSuccessful(true);
             loginResultResponse.setUserEntity(tokenEntity.getUser());
             loginResultResponse.setUuid(tokenEntity.getUuid());
-            return loginResultResponse;
+            return new ResponseEntity<LoginResultResponse>(loginResultResponse, HttpStatus.OK);
         } else {
-            httpServletResponse.sendError(406, "Email or Password are wrong");
-            return null;
+            return ResponseEntity.ok(loginFormRequest);
         }
+    }
+    @RequestMapping("/logout")
+    public ResponseEntity<?> logout(@RequestHeader("Authorization") UUID uuid){
+        UserEntity userEntity = tokenService.getUser(uuid);
+        userEntity.setToken(null);
+        userService.editUser(userEntity);
+        tokenService.deleteToken(uuid);
+
     }
 }
