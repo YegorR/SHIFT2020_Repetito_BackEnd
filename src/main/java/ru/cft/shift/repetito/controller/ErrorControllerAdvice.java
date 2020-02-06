c9eae2ba9906d5ca24cd9293017e7942539756bb
Ruslan <56080256+RuslanMac@users.noreply.github.com>
2020-02-06 16:52:22 +0700

    Change ErrorControllerAdvice

--- src/main/java/ru/cft/shift/repetito/controller/ErrorControllerAdvice.java
+++ src/main/java/ru/cft/shift/repetito/controller/ErrorControllerAdvice.java
@@ -6,6 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import ru.cft.shift.repetito.exception.AccessIsForbiddenException;
+import ru.cft.shift.repetito.exception.NotAuthorisedException;
+import ru.cft.shift.repetito.exception.ResourceIsNotFoundException;
 
 
 @ControllerAdvice
@@ -19,6 +22,17 @@ public class ErrorControllerAdvice {
         responseStatus.setCode(ResponseCode.ERROR.getCode());
         responseStatus.setErrorMessage("Сервис временно недоступен. Попробуйте позже.");
         resultResponse.setStatus(responseStatus);*/
+        if (e instanceof ResourceIsNotFoundException){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Resource is not found");
+        }
+        else if (e instanceof AccessIsForbiddenException){
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Access is forbidden");
+        }
+        else if (e instanceof NotAuthorisedException){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Not authorised");
+        }
+
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Server error");
+
     }
 }
