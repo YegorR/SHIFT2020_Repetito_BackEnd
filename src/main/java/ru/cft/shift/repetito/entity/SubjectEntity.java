625c1303f10673b6fb3402411e44b6ff45dfd507
gft <gfhftht>
2020-02-06 14:09:00 +0700

    Add Subject logic

---
+++ src/main/java/ru/cft/shift/repetito/entity/SubjectEntity.java
@@ -0,0 +1,46 @@
+package ru.cft.shift.repetito.entity;
+
+import javax.persistence.*;
+
+import java.util.Set;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table("subjects")
+public class SubjectEntity {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="subject")
+    private String name;
+
+    public Set<UserEntity> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<UserEntity> users) {
+        this.users = users;
+    }
+
+    @ManyToMany
+    private Set<UserEntity> users;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
