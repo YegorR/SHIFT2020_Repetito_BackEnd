419a260b11ccab442c81e15f9e61acd62a39fba2
Hutlcot <vorontsov.r@bk.ru>
2020-02-07 16:38:48 +0700

    подредачил UserEntity

--- src/main/java/ru/cft/shift/repetito/entity/UserEntity.java
+++ src/main/java/ru/cft/shift/repetito/entity/UserEntity.java
@@ -78,7 +78,7 @@ public class UserEntity {
         this.patronymic = userParamsRequest.getPatronymic();
         this.faculty = userParamsRequest.getFaculty();
         this.course = userParamsRequest.getCourse();
-        this.teacher = userParamsRequest.getIsTeacher();
+        this.teacher = userParamsRequest.getTeacher();
         this.degree = userParamsRequest.getDegree();
         this.about = userParamsRequest.getAbout();
         this.price=userParamsRequest.getPrice();
